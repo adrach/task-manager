a529cd0f3ea2bb35bf0747d25b39cb490303f7e5
Andrew Drach <andrew.drach@yahoo.com>
2019-02-16 15:29:53 -0500

    update navbar, start prototyping task columns

--- app/javascript/components/tasks/TasksContainer.jsx
+++ app/javascript/components/tasks/TasksContainer.jsx
@@ -7,16 +7,45 @@ class TasksContainer extends React.Component {
   // }
 
   componentDidMount() {
-    window.console.log('success!');
+    const { greeting } = this.props;
+    window.console.log(greeting);
   }
 
   render() {
-    const { greeting } = this.props;
     return (
-      <div>
-        Greeting:
-        {' '}
-        {greeting}
+      <div className="container">
+        {/* <div className="row">
+          <div className="col">Column 1</div>
+          <div className="col">Column 2</div>
+          <div className="col">Column 3</div>
+          <div className="col">Column 4</div>
+          <div className="col">Column 5</div>
+        </div> */}
+        <div className="row">
+          <div className="d-flex flex-wrap w-100">
+            <div className="card card-body w-20">
+            Column 1
+            </div>
+            <div className="card card-body w-20">
+            Column 2
+            </div>
+            <div className="card card-body w-20">
+            Column 3
+            </div>
+            <div className="card card-body w-20">
+            Column 4
+            </div>
+            <div className="card card-body w-20">
+            Column 5
+            </div>
+            <div className="card card-body w-20">
+            Column 6
+            </div>
+            <div className="card card-body w-20">
+            Column 7
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
